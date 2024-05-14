d05f1735a2530a293769da8b231537863528c4bf
tschelovek <az18rg@gmail.com>
2024-05-14 13:40:48 +0300

    iframe, slider fixes in work

--- js/main.js
+++ js/main.js
@@ -19,35 +19,35 @@ document.addEventListener('DOMContentLoaded', () => {
     // document.querySelectorAll('.callback_init')
     //   .forEach(button => button.addEventListener('click', e => handlerOpenCallback(e.currentTarget)));
 
-    document.form_callback.addEventListener('submit', async e => {
-        e.preventDefault();
-
-        const form = e.target;
-        const messageContainer = form.querySelector('.callback__status');
-
-        messageContainer.textContent = '';
-        messageContainer.style.color = 'inherit'
-
-        try {
-            const myMessage = getStringFromInputs();
-
-            await postData(POST_FORM_URL, {message: myMessage})
-                .then((res) => {
-                    if (res.ok) {
-                        messageContainer.textContent = SUCCESS_MESSAGE;
-                        // очистка формы в случае успеха
-                        form.reset();
-                    } else {
-                        messageContainer.textContent = ERROR_MESSAGE;
-                        messageContainer.style.color = 'red'
-                    }
-                })
-        } catch (err) {
-            messageContainer.textContent = ERROR_MESSAGE;
-            messageContainer.style.color = 'red'
-            console.error(err);
-        }
-    })
+    // document.form_callback.addEventListener('submit', async (e) => {
+    //     e.preventDefault();
+    //
+    //     const form = e.target;
+    //     const messageContainer = form.querySelector('.callback__status');
+    //
+    //     messageContainer.textContent = '';
+    //     messageContainer.style.color = 'inherit'
+    //
+    //     try {
+    //         const myMessage = getStringFromInputs();
+    //
+    //         await postData(POST_FORM_URL, {message: myMessage})
+    //             .then((res) => {
+    //                 if (res.ok) {
+    //                     messageContainer.textContent = SUCCESS_MESSAGE;
+    //                     // очистка формы в случае успеха
+    //                     form.reset();
+    //                 } else {
+    //                     messageContainer.textContent = ERROR_MESSAGE;
+    //                     messageContainer.style.color = 'red'
+    //                 }
+    //             })
+    //     } catch (err) {
+    //         messageContainer.textContent = ERROR_MESSAGE;
+    //         messageContainer.style.color = 'red'
+    //         console.error(err);
+    //     }
+    // })
 
     function handlerOpenCallback(eTarget) {
         modal.style.display = 'block';
@@ -72,38 +72,47 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(() => modal.style.display = 'none', 600);
     }
 
-    async function postData(url = '', data = {}) {
-        await fetch(url, {
-            method: 'POST',
-            mode: 'cors',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-            .catch(() => {
-                throw Error
-            })
-    }
+    // async function postData(url = '', data = {}) {
+    //     await fetch(url, {
+    //         method: 'POST',
+    //         mode: 'cors',
+    //         credentials: 'same-origin',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(data)
+    //     })
+    //         .catch(() => {
+    //             throw Error
+    //         })
+    // }
 
-    function getStringFromInputs() {
-        const inputNameValue = document.getElementById('callback_name')?.value.trim();
-        const inputEmailValue = document.getElementById('callback_email')?.value.trim();
-        const inputPhoneValue = document.getElementById('callback_tel')?.value.trim();
-
-        return `${inputNameValue}, ${inputPhoneValue}, ${inputEmailValue}`
-    }
+    // function getStringFromInputs() {
+    //     const inputNameValue = document.getElementById('callback_name')?.value.trim();
+    //     const inputEmailValue = document.getElementById('callback_email')?.value.trim();
+    //     const inputPhoneValue = document.getElementById('callback_tel')?.value.trim();
+    //
+    //     return `${inputNameValue}, ${inputPhoneValue}, ${inputEmailValue}`
+    // }
 
     /**
      *
      * Бургер меню
      *
      */
-    document.getElementById('burgerButton')?.addEventListener('click', e => {
+
+    const navMenu = document.querySelector('.header__right-block');
+
+    document.getElementById('burgerButton').addEventListener('click', function (e) {
         e.preventDefault();
         e.currentTarget.classList.toggle('active');
-        document.querySelector('.header__right-block')?.classList.toggle('active');
+        navMenu.classList.toggle('active');
+
+        if (navMenu.classList.contains('active')) {
+            setTimeout(() => document.addEventListener('click', outClickHandler), 10)
+        } else {
+            document.removeEventListener('click', outClickHandler)
+        }
     })
 
     document.querySelectorAll('.nav_header .header__link')
@@ -112,6 +121,24 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementById('burgerButton').classList.remove('active');
             }
         ))
+
+    // navMenu.addEventListener('click', e => {
+    //   if (e.target === e.currentTarget) handlerCloseModal()
+    // });
+
+    function outClickHandler(e) {
+        // console.log(e.currentTarget.closest('.header__right-block'))
+        console.log(e.target)
+        if (e.target !== navMenu && e.target.closest('.header__right-block') !== navMenu) {
+            closeMenu()
+            console.log('tut zakryvaem')
+        }
+    }
+
+    function closeMenu() {
+
+    }
+
 
     /**
      *
@@ -128,6 +155,11 @@ document.addEventListener('DOMContentLoaded', () => {
             delay: 2000,
         },
         speed: 1000,
+        // navigation: {
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        // },
+
         breakpoints: {
             480: {
                 navigation: {
@@ -138,7 +170,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
-
     /**
      *
      * Анимация переворота карточек .systema__item
@@ -147,7 +178,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const animFlipParams = {
         options: {duration: 400, fill: "both", direction: 'normal'},
         keyframesIn: [
-            {transform: 'rotateY()', easing: 'cubic-bezier(0.455, 0.030, 0.515, 0.955)'},
+            {transform: 'rotateY(0deg)', easing: 'cubic-bezier(0.455, 0.030, 0.515, 0.955)'},
             {transform: 'rotateY(180deg)', easing: 'cubic-bezier(0.455, 0.030, 0.515, 0.955)'},
         ],
         keyframesOut: [
@@ -155,20 +186,26 @@ document.addEventListener('DOMContentLoaded', () => {
             {transform: 'rotateY(360deg)', easing: 'cubic-bezier(0.455, 0.030, 0.515, 0.955)'},
         ]
     }
+    const systemaItems = document.querySelectorAll('.systema__item');
 
-    document.querySelectorAll('.systema__item')
-        .forEach(item => item
-            .addEventListener('click', () => {
-                item.classList.toggle('active');
-                item.classList.contains('active')
-                    ? item.animate([...animFlipParams.keyframesIn], {...animFlipParams.options})
-                    : item.animate([...animFlipParams.keyframesOut], {...animFlipParams.options})
-            })
-        )
+    systemaItems.forEach(item => item
+        .addEventListener('click', () => {
+            if (item.classList.contains('flip-vertical-left')) {
+                item.classList.add('backflip-vertical-left');
+                setTimeout(() => {
+                    item.classList.remove('flip-vertical-left');
+                    item.classList.remove('backflip-vertical-left');
+                }, 450)
+            } else {
+                item.classList.add('flip-vertical-left');
+            }
+        })
+    )
 
     /**
      * Масштабирование айфреймов по ширине обёртки
      */
+        // import ResizeObserver from './libs/ResizeObserver';
 
     const FULL_IFRAME_WIDTH_RSCIENCE = 1105;
     const FULL_IFRAME_WIDTH_2HARD = 1156;
@@ -176,6 +213,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const rocketScience = document.querySelector('.iframe__wrapper_rocketscience');
     const tooHard = document.querySelector('.iframe__wrapper_2hard');
     const hive = document.querySelector('.iframe__wrapper_hive');
+
+    if (!('ResizeObserver' in window)) {
+        import('./libs/ResizeObserver')
+    }
+    console.log(ResizeObserver)
+    console.log( new ResizeObserver(()=> {}))
+    console.log('text')
 
     new ResizeObserver(() => fitIframeToWindow(rocketScience, FULL_IFRAME_WIDTH_RSCIENCE)).observe(rocketScience);
     new ResizeObserver(() => fitIframeToWindow(tooHard, FULL_IFRAME_WIDTH_2HARD)).observe(tooHard);
